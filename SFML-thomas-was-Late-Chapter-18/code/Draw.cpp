bb64d239832e23dcc5af8f773cb7c1d5ae251897
tobywomack04 <toby.womack9.tw@gmail.com>
2025-10-14 14:11:07 +0100

    Add project files.

---
+++ SFML-thomas-was-Late-Chapter-18/code/Draw.cpp
@@ -0,0 +1,46 @@
+#include "Engine.h"
+
+void Engine::draw()
+{
+	// Rub out the last frame
+	m_Window.clear(Color::White);
+	
+	// Update the shader parameters
+	m_RippleShader.setUniform("uTime", m_GameTimeTotal.asSeconds());
+
+	// Switch to background view
+	m_Window.setView(m_BGMainView);
+	// Draw the background
+	m_Window.draw(m_BackgroundSprite);
+
+	// Draw the background, complete with shader effect
+	m_Window.draw(m_BackgroundSprite, &m_RippleShader);
+
+	// Switch to m_MainView
+	m_Window.setView(m_MainView);		
+
+	// Draw the Level
+	m_Window.draw(m_VALevel, &m_TextureTiles);
+
+	// Draw thomas
+	m_Window.draw(m_Thomas.getSprite());
+
+	// Draw the particle system
+	if (m_PS.running())
+	{
+		m_Window.draw(m_PS);
+	}
+	
+	// Draw the HUD
+	// Switch to m_HudView
+	m_Window.setView(m_HudView);
+	m_Window.draw(m_Hud.getLevel());
+	m_Window.draw(m_Hud.getTime());
+	if (!m_Playing)
+	{
+		m_Window.draw(m_Hud.getMessage());
+	}
+	
+	// Show everything we have just drawn
+	m_Window.display();
+}
---
+++ SFML-thomas-was-Late-Chapter-18/code/Input.cpp
@@ -0,0 +1,30 @@
+#include "Engine.h"
+
+void Engine::input()
+{
+	Event event;
+	while (m_Window.pollEvent(event))
+	{
+		if (event.type == Event::KeyPressed)
+		{
+			// Handle the player quitting
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
+			{
+				m_Window.close();
+			}
+
+			// Handle the player starting the game
+			if (Keyboard::isKeyPressed(Keyboard::Return))
+			{
+				m_Playing = true;
+			}
+		}
+	}	
+
+	// Handle input specific to Thomas
+	if (m_Thomas.handleInput())
+	{
+		// Play a jump sound
+		m_SM.playJump();
+	}
+}
